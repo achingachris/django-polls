046cdee9f324efbeef7c510fdcad7ecfa1d7d619
ca <achinga.chris@gmail.com>
2024-11-04 17:14:28 +0300

    Add Polls app configuration and initial models with migrations

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    """
+    A model representing a poll question.
+    Attributes:
+        question_text (CharField): The text of the question.
+        pub_date (DateTimeField): The date and time when the question was published.
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    """
+    Represents a choice for a question in a poll.
+    Attributes:
+        question (ForeignKey): The question this choice is associated with.
+        choice_text (CharField): The text of the choice.
+        votes (IntegerField): The number of votes this choice has received.
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
